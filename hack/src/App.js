6fc665f8984f326bfe98054d96f9d5a576ed182a
poomktw <kittiwat.ktw@mail.kmutt.ac.th>
2019-10-29 14:24:00 +0700

    [UPDATE] add button

--- hack/src/App.js
+++ hack/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Button from './components/Button';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Button />
     </div>
   );
 }
